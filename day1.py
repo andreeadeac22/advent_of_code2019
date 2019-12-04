1a2e4ca1d0c45a4935d2df3c6e5d627b09daf9ed
andreeadeac22 <andreeadeac22@gmail.com>
2019-12-03 23:41:03 -0500

    Day 1

--- day1.py
+++ day1.py
@@ -1,1 +1,12 @@
 # The Tyranny of the Rocket Equation
+
+fin = open("day1_input.txt", "r")
+lines = fin.readlines()
+
+total = 0
+for line in lines:
+  el = line.split()
+  if len(el) > 0:  
+    total += int(el[0]) // 3 - 2
+
+print(total)
