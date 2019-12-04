a16fe56aff7aea0a786e6847f1a71e3f1f043026
andreeadeac22 <andreeadeac22@gmail.com>
2019-12-04 00:58:58 -0500

    Day 2, part2:

--- day2.py
+++ day2.py
@@ -2,18 +2,28 @@ fin = open("input_day2.txt", "r")
 
 for line in fin:
   arr = line.split(",")
-arr = [int(x) for x in arr]
+orig_arr = [int(x) for x in arr]
 
-arr[1] = 12
-arr[2] = 2
+def compute(n,v):
+	arr = list(orig_arr)
+	arr[1] = n
+	arr[2] = v
+	pos = 0
+	while pos+3 < len(arr) and arr[pos] != 99:
+	  if arr[pos] == 1:
+	    arr[arr[pos+3]] = arr[arr[pos+1]] + arr[arr[pos+2]]
+	  elif arr[pos] == 2:
+	    arr[arr[pos+3]] = arr[arr[pos+1]] * arr[arr[pos+2]]
+	  else:
+	   break
+	  pos +=4
+	return arr[0]
 
-pos = 0
-while pos+3 < len(arr) and arr[pos] != 99:
-  if arr[pos] == 1:
-    arr[arr[pos+3]] = arr[arr[pos+1]] + arr[arr[pos+2]]
-  elif arr[pos] == 2:
-    arr[arr[pos+3]] = arr[arr[pos+1]] * arr[arr[pos+2]]
-  else:
-   break
-  pos +=4
-print(arr[0])
+for n in range(100):
+  for v in range(100):
+    
+    res = compute(n,v)
+    print(res)    
+    if res == 19690720:
+      print("Value ", 100 * n + v) 
+      exit(0)
