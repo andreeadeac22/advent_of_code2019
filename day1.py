094a9da3dfd46ef331f8b3ce82e0e27e9b60e5c4
andreeadeac22 <andreeadeac22@gmail.com>
2019-12-04 00:00:08 -0500

    Day 1, part 2

--- day1.py
+++ day1.py
@@ -10,3 +10,17 @@ for line in lines:
     total += int(el[0]) // 3 - 2
 
 print(total)
+
+# part 2
+
+total = 0
+for line in lines:
+  el = line.split()
+  if len(el) > 0:
+    x = int(el[0])
+    x = x // 3 - 2
+    while x > 0:
+      total += x
+      x = x // 3 - 2
+
+print(total)
