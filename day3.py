1efd42553e961e04df666fb0e36d00f573000a10
andreeadeac22 <andreeadeac22@gmail.com>
2019-12-08 00:28:13 -0500

    Day 3, part2

--- day3.py
+++ day3.py
@@ -3,8 +3,10 @@ lines = file.readlines()
 arr1 = lines[0].split(",")
 arr2 = lines[1].split(",")
 
-def add_positions(arr, pos, dir, len):
+def add_positions(arr, pos, dir, len, di=None, total_pos=None):
   for i in range(len):
+    if total_pos is not None:
+        total_pos +=1 
     if dir == 'R':
       pos = (pos[0] + 1, pos[1]) 
     if dir == 'U':
@@ -14,23 +16,28 @@ def add_positions(arr, pos, dir, len):
     if dir == 'D':
       pos = (pos[0], pos[1] - 1)
     arr.append(pos)
-  return arr, pos
+    di[pos] = total_pos
+  return arr, pos, di, total_pos
 
 
 positions1 = [(0,0)]
 pos1 = (0,0)
 pos2 = (0,0)
 positions2 = [(0,0)]
+di1 = {}
+di2 = {}
+total_pos1 = 0
+total_pos2 = 0
 # R x+, U y+, L x-, D y-
 for el in arr1:
   dir = el[0]
   length = int(el[1:])
-  positions1, pos1 = add_positions(positions1, pos1, dir, length)
+  positions1, pos1, di1, total_pos1 = add_positions(positions1, pos1, dir, length, di1, total_pos1)
 
 for el in arr2:
   dir = el[0]
   length = int(el[1:])
-  positions2, pos2 = add_positions(positions2, pos2, dir, length)
+  positions2, pos2, di2, total_pos2  = add_positions(positions2, pos2, dir, length, di2, total_pos2)
 
 positions1.sort()
 positions2.sort()
@@ -46,8 +53,9 @@ def inters(p1, p2):
       j += 1
     else:
        assert p1[i][0] == p2[j][0] and p1[i][1] == p2[j][1], ("{}.{}".format(p1[i], p2[j]))
-       if (p1[i][0] != 0 or p1[i][1] != 0) and abs(p1[i][0]) + abs(p1[i][1]) < min:
-         min = abs(p1[i][0]) + abs(p1[i][1])
+       #if (p1[i][0] != 0 or p1[i][1] != 0) and abs(p1[i][0]) + abs(p1[i][1]) < min:
+       if  (p1[i][0] != 0 or p1[i][1] != 0) and di1[p1[i]] + di2[p2[j]] < min:
+        min = di1[p1[i]] + di2[p2[j]]
        i += 1
        j += 1
   return min
