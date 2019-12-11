15702bccafaed173dc3f85896e467a3e67f459d8
andreeadeac22 <andreeadeac22@gmail.com>
2019-12-10 20:05:47 -0500

    Day 6, part2

--- day6.py
+++ day6.py
@@ -4,12 +4,11 @@ lines = fin.read().splitlines()
 notzd = set() # set of ids which don't have zero in_degree
 id_node_dict = {} #XYZ - id
 neigh_node = {} # id_from : id_to
-
+node_id_dict = {} # id - XYZ
 
 num_node = 0 
 for line in lines:
     nodes = line.split(')')
-    print(nodes)
     n1 = nodes[0]
     n2 = nodes[1]
     if n1 not in id_node_dict:
@@ -21,12 +20,19 @@ for line in lines:
     idn1 = id_node_dict[n1]
     idn2 = id_node_dict[n2]
     
-    neigh_node[idn2] = idn1 
+    node_id_dict[idn1] = n1
+    node_id_dict[idn2] = n2
+    
+    if idn2 not in neigh_node:
+      neigh_node[idn2] = [idn1]
+    else:
+      neigh_node[idn2].append(idn1)
+    # for part 2
+    if idn1 not in neigh_node:
+       neigh_node[idn1] = [idn2]
+    else:
+      neigh_node[idn1].append(idn2)   
     notzd.add(idn1)
-
-print(notzd)
-print(id_node_dict)
-print(neigh_node)
 
 orbits = num_node * [0]  # o[node_id] = num_orbits 
 
@@ -41,7 +47,7 @@ def dfs(root, orbits):
     orbits[root] = res
     return res
 
-
+"""
 for i in range(num_node):
     if i not in notzd:
         # end node
@@ -49,3 +55,25 @@ for i in range(num_node):
         #print(sum(orbits))
 
 print(sum(orbits))
+"""
+
+dist = num_node * [0]
+def bfs(root, dist):
+  dist[root] = 1
+  visited = []
+  visited.append(root)
+  curr_pos = 0
+  last = 0
+  while curr_pos <= last:
+    if visited[curr_pos] in neigh_node:
+      for neigh in neigh_node[visited[curr_pos]]:
+        if neigh == id_node_dict['SAN']:
+          return dist[visited[curr_pos]] + 1
+        if dist[neigh] == 0:
+          dist[neigh] = dist[visited[curr_pos]] + 1
+          last += 1
+          visited.append(neigh)
+    curr_pos +=1
+  return dist[id_node_dict['SAN']]
+
+print(bfs(id_node_dict['YOU'], dist))
